53f3bcb036a6a21d227f4bfcfb5d00d65fe9cd82
Kyle Ferguson <kylegferg@gmail.com>
2023-05-07 14:13:04 -0400

    fix matrix

--- dependency-matrix.sh
+++ dependency-matrix.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 getPhpVersion () {
   php -r "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;"
 }
@@ -9,14 +11,15 @@ cleanup () {
   rm -rf vendor || true
 }
 
-installLaravel (version) {
-  composer require laravel/framework:"${version}" --dev --with-all-dependencies
+installLaravel () {
+  composer require laravel/framework:"$1" --dev --with-all-dependencies
+  composer update
 }
 
-PHP_74_LARAVEL=("^7.0", "^8.0")
-PHP_80_LARAVEL=("^7.0", "^8.0", "^9.0")
-PHP_81_LARAVEL=("^8.0", "^9.0", "^10.0")
-PHP_82_LARAVEL=("^8.0", "^9.0", "^10.0")
+PHP_74_LARAVEL=("^7.0" "^8.0")
+PHP_80_LARAVEL=("^7.0" "^8.0" "^9.0")
+PHP_81_LARAVEL=("^8.0" "^9.0" "^10.0")
+PHP_82_LARAVEL=("^8.0" "^9.0" "^10.0")
 
 if [ "$(getPhpVersion)" = "7.4" ]
 then
@@ -25,7 +28,7 @@ then
   do
     echo "### Test Laravel ${version} ###"
     cleanup
-    installLaravel(version)
+    installLaravel $version
     composer run-script test
   done
 fi
@@ -37,7 +40,7 @@ then
   do
     echo "### Test Laravel ${version} ###"
     cleanup
-    installLaravel(version)
+    installLaravel $version
     composer run-script test
   done
 fi
@@ -49,7 +52,7 @@ then
   do
     echo "### Test Laravel ${version} ###"
     cleanup
-    installLaravel(version)
+    installLaravel $version
     composer run-script test
   done
 fi
@@ -61,7 +64,7 @@ then
   do
     echo "### Test Laravel ${version} ###"
     cleanup
-    installLaravel(version)
+    installLaravel $version
     composer run-script test
   done
 fi
